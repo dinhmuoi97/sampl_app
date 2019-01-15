95c590b0cbd3a747cb993836e7b45af60174133b
dinhmuoi97 <dinhmuoi1997@gmail.com>
2019-01-15 15:21:17 +0700

    chapter 8: basic login

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
-  get "/login", to: "static_pages#login"
+  get "/login", to: "sessions#new"
   get "/home", to: "static_pages#home"
   post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 end
