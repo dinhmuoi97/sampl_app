f342419c826b6a3accfc23ac2cd1b2a823cd2cbc
dinhmuoi97 <dinhmuoi1997@gmail.com>
2019-01-15 15:21:17 +0700

    chapter 7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {maximun: Settings.max_user}
+  validates :name, presence: true, length: {maximum: Settings.max_user}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximun: Settings.max_email},
+  validates :email, presence: true, length: {maximum: Settings.max_email},
   format: {with: VALID_EMAIL_REGEX},
   uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimun: Settings.max_pass}
+  validates :password, presence: true, length: {minimum: Settings.mini_pass}
 end
